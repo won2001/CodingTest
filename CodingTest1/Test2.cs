a4e9f46c009ada5a43343553ac204f4fa10465e2
won2001 <djdjvlf2001@naver.com>
2024-08-07 23:51:52 +0900

    코딩 테스트 0단계
    코딩 테스트 문제 풀이

---
+++ CodingTest1/Test2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest1
+{
+    //배열 뒤집기
+    public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            Array.Reverse(num_list);
+            return num_list;
+        }
+    }
+    //다른사람 풀이
+    /*public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            int[] answer = new int[num_list.Length];
+            int K = 0;
+            for (int i = num_list.Length - 1; i >= 0; i--)
+            {
+                answer[K] = num_list[i];
+                K++;
+            }
+            return answer;
+        }
+    }*/
+}
